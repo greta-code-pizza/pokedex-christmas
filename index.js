91ab5052c1bb89a12ae2b34cf4bc87ed17200ac0
timote56 <alexandreroffiaen@gmail.com>
2021-01-04 14:08:43 +0100

    ajout variable classname

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ let pokemonType = document.getElementsByClassName("card");
 
 const plantsType = document.getElementsByClassName("fa-leaf");
 const poisonType = document.getElementsByClassName("fa-skull-crossbones");
+const waterType = document.getElementsByClassName("fas fa-water");
+const fireType = document.getElementsByClassName("fas fa-fire");
+const pawType = document.getElementsByClassName("fas fa-paw");
+const infoType = document.getElementsByClassName("fas fa-feather-alt");
+const insectType = document.getElementsByClassName("fas fa-bug");
 
 pokemonType.addEventListener('onclick', function type() {
     if () {
